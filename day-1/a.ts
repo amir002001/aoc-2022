280a17f290b6a28bf08927faa1df1889d9fc95a3
Amir Afshari <a_amirhossein@icloud.com>
2022-12-01 00:09:12 -0500

    part 1 done 😤😤

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,3 +1,18 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+let results = input.split("\r\n").map(str => Number(str));
+
+let maxCalories = 0;
+let currentCalories = 0;
+for (let result of results) {
+  if (result === 0) {
+    maxCalories = Math.max(currentCalories, maxCalories)
+    currentCalories = 0;
+    continue;
+  }
+  currentCalories += result
+}
+
+console.log(maxCalories);

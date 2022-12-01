8b38e45d9b64e15918729450823b304a428a2fb9
Amir Afshari <a_amirhossein@icloud.com>
2022-12-01 00:18:22 -0500

    LFGGGGGGGGGGGGGG Day 2 done

--- day-1/b.ts
+++ day-1/b.ts
@@ -1,3 +1,20 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+let results = input.split("\r\n").map((str) => Number(str));
+
+let elfPacks: number[] = [];
+let currentCalories = 0;
+for (let result of results) {
+  if (result === 0) {
+    elfPacks.push(currentCalories);
+    currentCalories = 0;
+    continue;
+  }
+  currentCalories += result;
+}
+
+elfPacks.sort((n1: number, n2: number) => n2-n1);
+
+console.log(elfPacks[1]+elfPacks[2] + elfPacks[0]);

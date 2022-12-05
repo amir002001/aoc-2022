289df835afd07a67a7f625035646ea2d59542f5e
amir002001 <a_amirhossein@icloud.com>
2022-12-05 13:51:12 -0500

    day 3 done LFGGGG

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,3 +1,34 @@
 export {};
 
 const input = await Deno.readTextFile("./input.txt")
+
+
+const rucksacks = input.split("\n")
+
+let sum = 0;
+const A_UPPER = "A".charCodeAt(0);
+const A_LOWER = "a".charCodeAt(0);
+
+
+for(const rucksack of rucksacks) {
+    const first = rucksack.slice(0, rucksack.length/2)
+    const second = rucksack.slice(rucksack.length/2)
+    const firstSet = new Set(first.split(""))
+
+    for(const char of second) {
+        if(firstSet.has(char)) {
+            let priority = 0;
+            const charCode = char.charCodeAt(0)
+            if(A_UPPER + 26 > charCode && A_UPPER <= charCode) {
+                priority += charCode - A_UPPER + 27;
+            } else {
+                priority += charCode - A_LOWER + 1
+            }
+            sum += priority;
+            break
+        }
+    }
+    
+}
+console.log(sum)
+// console.log(rucksacks)
--- day-3/b.ts
+++ day-3/b.ts
@@ -1,3 +1,37 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+const rucksacks = input.split("\n");
+
+let sum = 0;
+const A_UPPER = "A".charCodeAt(0);
+const A_LOWER = "a".charCodeAt(0);
+
+let lastThree: String[] = [];
+let count = 0;
+for (const rucksack of rucksacks) {
+    lastThree.push(rucksack);
+
+    if (lastThree.length !== 3) continue
+
+    const set1 = new Set(lastThree[0]);
+    const set2 = new Set(lastThree[1]);
+
+    for (const char of lastThree[2]) {
+        if (set1.has(char) && set2.has(char)) {
+            let priority = 0;
+            const charCode = char.charCodeAt(0);
+            if (A_UPPER + 26 > charCode && A_UPPER <= charCode) {
+                priority += charCode - A_UPPER + 27;
+            } else {
+                priority += charCode - A_LOWER + 1;
+            }
+            sum += priority;
+            break;
+        }
+    }
+
+    lastThree = []
+}
+console.log(sum);

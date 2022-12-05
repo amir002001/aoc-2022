e660e5217aa59b77dfcf3df0feebe0f766ccb3d1
amir002001 <a_amirhossein@icloud.com>
2022-12-05 10:43:16 -0500

    day 2 done

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,3 +1,36 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+const rounds = input.split("\n").map((round) => round.split(" "));
+
+let total = 0;
+
+const themMap = new Map(["A", "B", "C"].map((val, i) => [val, i]));
+const meMap = new Map(["X", "Y", "Z"].map((val, i) => [val, i]));
+
+for (const [they, me] of rounds) {
+    const myMove = meMap.get(me)!;
+    const theirMove = themMap.get(they)!;
+
+    let roundTotal = 0;
+
+    switch (me) {
+        case "X":
+            roundTotal += 1;
+            break;
+        case "Y":
+            roundTotal += 2;
+            break;
+        case "Z":
+            roundTotal += 3;
+            break;
+    }
+
+    
+    if ((theirMove + 1) % 3 === myMove) roundTotal += 6;
+    else if(!((myMove + 1) % 3 === theirMove)) roundTotal += 3;
+
+    total += roundTotal
+}
+ console.log(total);
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,3 +1,45 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+const rounds = input.split("\n").map((round) => round.split(" "));
+
+let total = 0;
+
+const themMap = new Map(["A", "B", "C"].map((val, i) => [val, i]));
+
+for (const [they, scenario] of rounds) {
+    let myMove = 0;
+    const theirMove = themMap.get(they)!;
+
+    let roundTotal = 0;
+
+    switch (scenario) {
+        case "X":
+            myMove = (3 + theirMove - 1) % 3;
+            roundTotal += 0;
+            break;
+        case "Y":
+            roundTotal += 3;
+            myMove = theirMove;
+            break;
+        case "Z":
+            myMove = (theirMove + 1) % 3;
+            roundTotal += 6;
+            break;
+    }
+
+    switch (myMove) {
+        case 0:
+            roundTotal += 1
+            break;
+        case 1:
+            roundTotal += 2
+            break;
+        case 2:
+            roundTotal += 3
+            break;
+    }
+    total += roundTotal
+}
+console.log(total);
